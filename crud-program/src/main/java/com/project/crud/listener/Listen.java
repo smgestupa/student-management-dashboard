8cfc35b08a550f903f875536c3f18bb0d30d5489
Shawn Shawn <laazy.pr00gramming@protonmail.com>
2021-10-10 11:08:42 +0800

    removed the extra crud-program folder inside the main crud-program folder

--- crud-program/src/main/java/com/project/crud/listener/Listen.java
+++
@@ -1,9 +0,0 @@
-package com.project.crud.listener;
-
-import com.project.crud.model.Student;
-import javafx.scene.input.MouseEvent;
-
-public interface Listen {
-
-    public void onClickListener( MouseEvent event, Student student ); // creates an interface that can be used by the [StudentController] class
-}
